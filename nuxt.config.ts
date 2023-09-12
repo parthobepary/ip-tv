71b7c09f13bd55057710cf4339c00abad57dfbf7
beparypartho <pallobbiswas326@gmail.com>
2023-09-12 23:23:07 +0600

    add live-stream-player

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,44 +1,56 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    ssr: false,
-    srcDir: 'src/',
-    app: {
-        head: {
-            charset: 'utf-16',
-            viewport: 'width=500, initial-scale=1',
-            title: 'IP-TV',
-            meta: [
-                // <meta name="description" content="My amazing site">
-                {name: 'description', content: 'IP-TV'}
-            ],
-        }
-    },
-    imports: {
-        dirs: ['stores'],
-    },
-    css: ['~/assets/css/tailwind.css'],
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {},
+  ssr: false,
+  srcDir: "src/",
+  app: {
+    head: {
+      charset: "utf-16",
+      viewport: "width=500, initial-scale=1",
+      title: "IP-TV",
+      meta: [
+        // <meta name="description" content="My amazing site">
+        { name: "description", content: "IP-TV" },
+      ],
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        {
+          rel: "stylesheet",
+          href: "https://unpkg.com/cloudinary-video-player@1.5.3/dist/cld-video-player.min.css",
         },
+      ],
+      script: [
+        {
+          src: "https://unpkg.com/cloudinary-core@latest/cloudinary-core-shrinkwrap.min.js",
+        },
+        {
+          src: "https://unpkg.com/cloudinary-video-player@1.5.9/dist/cld-video-player.min.js",
+        },
+      ],
     },
-    modules: [
-        '@nuxtjs/tailwindcss',
-        '@pinia/nuxt'
-    ],
-    vite: {
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    // additionalData: '@use "@/assets/_colors.scss" as *;'
-                }
-            }
-        }
+  },
+  imports: {
+    dirs: ["stores"],
+  },
+  css: ["~/assets/css/tailwind.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
     },
-    runtimeConfig: {
-        public: {
-            apiBase: process.env.API_BASE_URL
-        }
-    }
-})
+  },
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // additionalData: '@use "@/assets/_colors.scss" as *;'
+        },
+      },
+    },
+  },
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.API_BASE_URL,
+    },
+  },
+});
