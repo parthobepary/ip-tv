8c82e42dee2ef7d79e7792c46dd95a9b319d03cb
beparypartho <pallobbiswas326@gmail.com>
2023-09-15 22:13:12 +0600

    video issue solved

--- src/components/AnotherVideo.vue
+++ src/components/AnotherVideo.vue
@@ -1,7 +1,12 @@
 <template>
-    <div class="bg-red-400 h-32 ">
+    <div class="">
         <div>
-
+            <video controls>
+                <source
+                    src="https://cssagor-api.stylezworld.net/storage/page/video/lgRWomTOnddXbuDdbHgNBlnOe06DBSf59gUpoxBj.mp4"
+                    type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
         </div>
     </div>
 </template>
